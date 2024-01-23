6a29a9d12aa5222f54836dbcb928d7d90e040412
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-01-23 14:18:53 +0300

    NorthwindApp-1 Created MVC app template solution

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NorthwindApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
