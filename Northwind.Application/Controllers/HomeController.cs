17ce7c0553fa13fcc98f237d78f25c8577a6912a
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-02-03 17:22:47 +0300

    NorthwindApp-6 Solved issue with https certificate

--- Northwind.Application/Controllers/HomeController.cs
+++ Northwind.Application/Controllers/HomeController.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.Data;
+using NorthwindApp.Identity;
 using NorthwindApp.Models;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NorthwindApp.Controllers
 {
@@ -9,11 +15,13 @@ namespace NorthwindApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly NorthwindDbContext _db;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger, NorthwindDbContext db)
+        public HomeController(ILogger<HomeController> logger, NorthwindDbContext db, IConfiguration config)
         {
             _logger = logger;
             _db = db;
+            _config = config;
         }
 
         public IActionResult Index()
@@ -21,9 +29,34 @@ namespace NorthwindApp.Controllers
             return View();
         }
 
+        [Authorize]
+        [RequiresClaim("admin", "Admin")]
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Authorize()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetToken([FromForm] AuthorizationViewModel model)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_config["SecretKey"]);
+
+            var claims = new List<Claim>
+            {
+                new (ClaimTypes.Role, model.UserRole)
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+            await HttpContext.SignInAsync(claimsPrincipal);
+
+            return new OkObjectResult(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Northwind.Application/Models/AuthorizationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NorthwindApp.Models
+{
+    public class AuthorizationViewModel
+    {
+        public string UserName {  get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
