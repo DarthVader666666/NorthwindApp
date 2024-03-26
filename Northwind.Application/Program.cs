7d1c62fec1063dcd82675a3feaf8a0f7095024ec
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-03-26 12:02:22 +0300

    Resolved https certificate for docker-compose issue

--- Northwind.Application/Program.cs
+++ Northwind.Application/Program.cs
@@ -44,14 +44,14 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-//}
+}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
