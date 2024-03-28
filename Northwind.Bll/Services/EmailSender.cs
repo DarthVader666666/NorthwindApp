19e3663f4ba62ac9c9cf5e154f113e2dee76dcd8
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-03-28 14:19:45 +0300

    Commented emai sender entrails

--- Northwind.Bll/Services/EmailSender.cs
+++ Northwind.Bll/Services/EmailSender.cs
@@ -30,19 +30,19 @@ namespace Northwind.Bll.Services
             //    IsBodyHtml = true
             //};
 
-            string smtpAuthUsername = "azure-vader-communication-service";
-            string smtpAuthPassword = "I7O8Q~q1c2We3k-kOccmKaoeZpSr-Fk.5oJX3dg0";
-            string sender = "DoNotReply@00a2ce84-68f9-47ff-a5f8-cc87e989435c.azurecomm.net";
-            string smtpHostUrl = "smtp.azurecomm.net";
+            //string smtpAuthUsername = "azure-vader-communication-service";
+            //string smtpAuthPassword = "I7O8Q~q1c2We3k-kOccmKaoeZpSr-Fk.5oJX3dg0";
+            //string sender = "DoNotReply@00a2ce84-68f9-47ff-a5f8-cc87e989435c.azurecomm.net";
+            //string smtpHostUrl = "smtp.azurecomm.net";
 
-            var client = new SmtpClient(smtpHostUrl)
-            {
-                Port = 587,
-                Credentials = new NetworkCredential(smtpAuthUsername, smtpAuthPassword),
-                EnableSsl = true
-            };
+            //var client = new SmtpClient(smtpHostUrl)
+            //{
+            //    Port = 587,
+            //    Credentials = new NetworkCredential(smtpAuthUsername, smtpAuthPassword),
+            //    EnableSsl = true
+            //};
 
-            await client.SendMailAsync(new MailMessage(sender, email, subject, message));
+            //await client.SendMailAsync(new MailMessage(sender, email, subject, message));
         }
     }
 }
