e11ce97cd825702bea722f87afeb0fbba5519940
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-04-04 13:59:15 +0300

    Added migration to create admin user and role

---
+++ Northwind.Bll/Services/SqlScriptGenerator.cs
@@ -0,0 +1,25 @@
+﻿namespace Northwind.Bll.Services
+{
+    public static class SqlScriptGenerator
+    {
+        public static void GenerateAdminScript(string path, string email, string password, string securityStamp, string concurrencyStamp)
+        {
+            var sqlScript = 
+                $"INSERT INTO [dbo].[AspNetUsers] ([Id],[UserName],[NormalizedUserName],[Email],[NormalizedEmail],[EmailConfirmed],[PasswordHash]," +
+                $"[SecurityStamp],[ConcurrencyStamp],[PhoneNumber],[PhoneNumberConfirmed],[TwoFactorEnabled],[LockoutEnd],[LockoutEnabled],[AccessFailedCount]) " +
+                $"VALUES ('a5dcae83-a142-4ca6-a4ca-a85ac595d471','{email}','{email.ToUpper()}','{email}','{email.ToUpper()}',1," +
+                $"'{password}','{securityStamp}','{concurrencyStamp}',null,0,0,null,1,0) " +
+                $"INSERT INTO [dbo].[AspNetRoles]([Id],[Name],[NormalizedName],[ConcurrencyStamp]) VALUES ('00fba8d9-b4d3-4ec4-b39f-887a5d35cbfd','admin'," +
+                $"'ADMIN',null) " +
+                $"INSERT INTO [dbo].[AspNetUserRoles] ([UserId],[RoleId]) VALUES ('a5dcae83-a142-4ca6-a4ca-a85ac595d471', '00fba8d9-b4d3-4ec4-b39f-887a5d35cbfd')";
+
+            if (!File.Exists(path))
+            {
+                using (var sw = new StreamWriter(path, false))
+                {
+                    sw.Write(sqlScript);
+                }
+            }
+        }
+    }
+}
