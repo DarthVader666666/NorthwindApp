f15f6aa330266469c905146007c3cf020b078553
Vadzim Rumiantsau <rumyancer@gmail.com>
2024-05-07 12:11:53 +0300

    Refactored OrderController, _CreateFormPartial, added property to CustomerRepository

--- Northwind.Bll/Services/CustomerRepository.cs
+++ Northwind.Bll/Services/CustomerRepository.cs
@@ -1,13 +1,25 @@
-﻿using Northwind.Bll.Interfaces;
+﻿using Microsoft.AspNetCore.Identity;
+using Northwind.Bll.Interfaces;
 using Northwind.Data;
 using Northwind.Data.Entities;
+using System.Security.Claims;
 
 namespace Northwind.Bll.Services
 {
     public class CustomerRepository : RepositoryBase<Customer, NorthwindDbContext>
     {
-        public CustomerRepository(NorthwindDbContext dbContext) : base(dbContext)
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public CustomerRepository(NorthwindDbContext dbContext, UserManager<IdentityUser> userManager) : base(dbContext)
         {
+            _userManager = userManager;
+        }
+
+        public async Task<Customer?> GetUserCustomer(ClaimsPrincipal claims)
+        { 
+            var user = await _userManager.GetUserAsync(claims);
+
+            return user == null ? null : DbContext.Customers.FirstOrDefault(x => x.UserId == user.Id);
         }
     }
 }
